1402d89bd84604d3c0e26ba41d0dd8ce510bf283
romo <robmoore121@gmail.com>
2021-01-14 22:47:41 +0800

    Reorder test methods
    Former-commit-id: e901d23453d57a498995bfff412cfe6cd54c75b8

--- lib/src/test/kotlin/org/jetbrains/fulltextsearch/search/IndexedDirectoryTest.kt
+++ lib/src/test/kotlin/org/jetbrains/fulltextsearch/search/IndexedDirectoryTest.kt
@@ -9,30 +9,41 @@ import org.junit.jupiter.api.Test
 
 class IndexedDirectoryTest {
     @Test
-    internal fun `doesn't send query matches event if no matches found`() {
-        val indexedDirectory = IndexedDirectory(
-            listOf(
-                IndexedFile("file-1.txt", "abracadabra"),
-                IndexedFile("file-2.txt", "nothing interesting")
+    internal fun `finds one match`() {
+        val indexedDirectory =
+            IndexedDirectory(
+                listOf(
+                    IndexedFile(
+                        "hello-world.txt",
+                        "hello world"
+                    )
+                )
+            )
+
+        val queryResults: List<QueryMatch> =
+            indexedDirectory.queryCaseSensitive("world")
+        assertThat(queryResults, hasSize(1))
+        assertThat(
+            queryResults, hasItems(
+                QueryMatch("hello-world.txt", 6)
             )
         )
-        val eventCounter = object : QueryMatchListener {
-            var eventCounter = 0
+    }
 
-            override fun onQueryMatches(matches: List<QueryMatch>) {
-                eventCounter++
-            }
+    @Test
+    internal fun `finds multiple matches`() {
+        val indexedDirectory =
+            IndexedDirectory(listOf(IndexedFile("file.txt", "abracadabra")))
 
-            fun numberOfReceivedEvents(): Int {
-                return eventCounter
-            }
-        }
-
-        runBlocking {
-            indexedDirectory.queryCaseSensitivesAsync("abra", eventCounter)
-        }
-
-        assertThat(eventCounter.numberOfReceivedEvents(), equalTo(1))
+        val queryResults: List<QueryMatch> =
+            indexedDirectory.queryCaseSensitive("abra")
+        assertThat(queryResults, hasSize(2))
+        assertThat(
+            queryResults, hasItems(
+                QueryMatch("file.txt", 0),
+                QueryMatch("file.txt", 7)
+            )
+        )
     }
 
     @Test
@@ -64,40 +75,29 @@ class IndexedDirectoryTest {
     }
 
     @Test
-    internal fun `finds multiple matches`() {
-        val indexedDirectory =
-            IndexedDirectory(listOf(IndexedFile("file.txt", "abracadabra")))
-
-        val queryResults: List<QueryMatch> =
-            indexedDirectory.queryCaseSensitive("abra")
-        assertThat(queryResults, hasSize(2))
-        assertThat(
-            queryResults, hasItems(
-                QueryMatch("file.txt", 0),
-                QueryMatch("file.txt", 7)
+    internal fun `doesn't send query matches event if no matches found`() {
+        val indexedDirectory = IndexedDirectory(
+            listOf(
+                IndexedFile("file-1.txt", "abracadabra"),
+                IndexedFile("file-2.txt", "nothing interesting")
             )
         )
-    }
+        val eventCounter = object : QueryMatchListener {
+            var eventCounter = 0
 
-    @Test
-    internal fun `finds one match`() {
-        val indexedDirectory =
-            IndexedDirectory(
-                listOf(
-                    IndexedFile(
-                        "hello-world.txt",
-                        "hello world"
-                    )
-                )
-            )
+            override fun onQueryMatches(matches: List<QueryMatch>) {
+                eventCounter++
+            }
 
-        val queryResults: List<QueryMatch> =
-            indexedDirectory.queryCaseSensitive("world")
-        assertThat(queryResults, hasSize(1))
-        assertThat(
-            queryResults, hasItems(
-                QueryMatch("hello-world.txt", 6)
-            )
-        )
+            fun numberOfReceivedEvents(): Int {
+                return eventCounter
+            }
+        }
+
+        runBlocking {
+            indexedDirectory.queryCaseSensitivesAsync("abra", eventCounter)
+        }
+
+        assertThat(eventCounter.numberOfReceivedEvents(), equalTo(1))
     }
 }
