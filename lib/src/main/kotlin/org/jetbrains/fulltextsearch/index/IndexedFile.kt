11da770c380523b9e63040745bef008d7baea854
romo <robmoore121@gmail.com>
2021-01-18 14:15:27 +0800

    For small files (<50 chars), the indexer creates a SuffixTreeIndexedFile instead of the NaiveIndexedFile
    Former-commit-id: 2479dc2cf008c8a97ad5f2d589018f6da6f94dfa

--- lib/src/main/kotlin/org/jetbrains/fulltextsearch/index/IndexedFile.kt
+++ lib/src/main/kotlin/org/jetbrains/fulltextsearch/index/IndexedFile.kt
@@ -2,6 +2,7 @@ package org.jetbrains.fulltextsearch.index
 
 import org.jetbrains.fulltextsearch.filesystem.Directory
 import org.jetbrains.fulltextsearch.index.naive.NaiveIndexedFile
+import org.jetbrains.fulltextsearch.index.suffixtree.SuffixTreeIndexedFile
 import org.jetbrains.fulltextsearch.search.QueryMatch
 import java.io.File
 
@@ -12,7 +13,13 @@ interface IndexedFile {
 
     companion object {
         fun buildFor(root: Directory, file: File): IndexedFile {
-            return NaiveIndexedFile(root.relativePathTo(file.path), file.readText())
+            val relativePath = root.relativePathTo(file.path)
+            val fileText = file.readText()
+            return if (fileText.length < 50) {
+                SuffixTreeIndexedFile(relativePath, fileText)
+            } else {
+                NaiveIndexedFile(relativePath, fileText)
+            }
         }
     }
 }
