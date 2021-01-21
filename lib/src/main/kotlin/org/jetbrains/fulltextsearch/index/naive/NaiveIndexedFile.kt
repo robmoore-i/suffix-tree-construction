37228b1568723863ea21182fd916d387de4422d8
romo <robmoore121@gmail.com>
2021-01-22 04:54:43 +0800

    Revert 'Use positive-lookahead for the regex in the NaiveIndexedFile, so that it captures, for example, two occurrences of 'aa' in 'aaa'.' - It returned values that weren't correct.
    Former-commit-id: 193eea77c6a856c10ddbaad7ae0253134c1958b6

--- lib/src/main/kotlin/org/jetbrains/fulltextsearch/index/naive/NaiveIndexedFile.kt
+++ lib/src/main/kotlin/org/jetbrains/fulltextsearch/index/naive/NaiveIndexedFile.kt
@@ -2,17 +2,13 @@ package org.jetbrains.fulltextsearch.index.naive
 
 import org.jetbrains.fulltextsearch.index.IndexedFile
 import org.jetbrains.fulltextsearch.search.QueryMatch
-import java.util.regex.Pattern
-import java.util.stream.Collectors
 
 class NaiveIndexedFile(private val path: String, private val fileText: String) : IndexedFile {
     override fun query(queryString: String): List<QueryMatch> {
-        val pattern: Pattern = Pattern.compile("(?=$queryString)")
-        val matcher = pattern.matcher(fileText)
-        return matcher.results()
-            .collect(Collectors.toList())
+        return Regex.fromLiteral(queryString)
+            .findAll(fileText)
+            .map { QueryMatch(path, it.range.first) }
             .toList()
-            .map { QueryMatch(path, it.start()) }
     }
 
     override fun path(): String = path
