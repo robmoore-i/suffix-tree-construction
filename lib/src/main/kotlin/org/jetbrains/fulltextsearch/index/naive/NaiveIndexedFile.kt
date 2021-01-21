b0bbaa4fc8f97c2b10309bf94f7376a0ca7dd5b0
romo <robmoore121@gmail.com>
2021-01-22 04:37:54 +0800

    Use positive-lookahead for the regex in the NaiveIndexedFile, so that it captures, for example, two occurrences of 'aa' in 'aaa'.
    Former-commit-id: 55a26a18e36d56b80680cd49277a3a8018f3040d

--- lib/src/main/kotlin/org/jetbrains/fulltextsearch/index/naive/NaiveIndexedFile.kt
+++ lib/src/main/kotlin/org/jetbrains/fulltextsearch/index/naive/NaiveIndexedFile.kt
@@ -2,13 +2,17 @@ package org.jetbrains.fulltextsearch.index.naive
 
 import org.jetbrains.fulltextsearch.index.IndexedFile
 import org.jetbrains.fulltextsearch.search.QueryMatch
+import java.util.regex.Pattern
+import java.util.stream.Collectors
 
 class NaiveIndexedFile(private val path: String, private val fileText: String) : IndexedFile {
     override fun query(queryString: String): List<QueryMatch> {
-        return Regex.fromLiteral(queryString)
-            .findAll(fileText)
-            .map { QueryMatch(path, it.range.first) }
+        val pattern: Pattern = Pattern.compile("(?=$queryString)")
+        val matcher = pattern.matcher(fileText)
+        return matcher.results()
+            .collect(Collectors.toList())
             .toList()
+            .map { QueryMatch(path, it.start()) }
     }
 
     override fun path(): String = path
