0fd595357e50a173aa68724dab2c2a4e73828db6
romo <robmoore121@gmail.com>
2021-01-23 00:09:23 +0800

    Add script for zipping the project
    Former-commit-id: 8c903b3fe5fc66e73210de2d65a897962726720d

---
+++ scripts/zip-project.sh
@@ -0,0 +1,11 @@
+# shellcheck disable=SC2010
+if [[ $(ls -al | grep -c settings.gradle) != "1" ]]; then
+  echo "You can use this script from the root of the full-text-search repository.
+Current working directory: $(pwd)"
+  exit 1
+fi
+
+project_dir_name=$(pwd | xargs basename)
+pushd ..
+zip -r "${project_dir_name}/full-text-search.zip" "$project_dir_name" -x '*.git*' -x '*example-input-directories*' -x '*.idea*' -x '*build*' -x '*.gradle*'
+popd || exit
