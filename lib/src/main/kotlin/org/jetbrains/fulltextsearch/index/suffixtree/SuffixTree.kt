000428e9ad241f00593b634cc5f68ce8d89b0e74
romo <robmoore121@gmail.com>
2021-01-24 21:54:05 +0800

    Extract ActivePoint inner class now that I have got the right behaviour for activeNode, activeLength and activeEdge.
    Former-commit-id: 635c0b3edf43e0894c8869177996d470ba62d770

--- lib/src/main/kotlin/org/jetbrains/fulltextsearch/index/suffixtree/SuffixTree.kt
+++ lib/src/main/kotlin/org/jetbrains/fulltextsearch/index/suffixtree/SuffixTree.kt
@@ -5,16 +5,10 @@ import java.util.*
 
 class SuffixTree {
     private var currentlyInsertedInput = ""
-
-    private val rootNode: Node = RootNode()
-
-    private var activeNode: Node = rootNode
-    private var activeLength = 0
-    private var activeEdge = 0
-
-    private var suffixLinkCandidate: Node? = null
-
     private var remainingSuffixes = 0
+
+    private val rootNode = RootNode()
+    private val activePoint = ActivePoint()
 
     companion object {
         fun ukkonenConstruction(input: String): SuffixTree {
@@ -30,84 +24,14 @@ class SuffixTree {
         // already.
         currentlyInsertedInput += c
 
-        // We only add suffix links within a phase, so we reset it at the start of this phase.
-        suffixLinkCandidate = null
-
         // There is now an additional suffix which is not yet explicit in the tree, so we increment
         // our counter for the number of remaining suffixes.
         remainingSuffixes++
 
-        while (remainingSuffixes > 0) {
-            if (activeLength == 0) {
-                // If we're at a node, point our active edge at the most recently added character in
-                // the text.
-                activeEdge = currentlyInsertedInput.length - 1
-            }
-
-            val activeEdgeChar = currentlyInsertedInput[activeEdge]
-            if (!activeNode.edges.containsKey(activeEdgeChar)) {
-                // If the active node doesn't yet have a child node corresponding to the next
-                // character, add one. When we perform a leaf insertion like this, we need to add a
-                // suffix link.
-                activeNode.edges[activeEdgeChar] = LeafNode()
-                addSuffixLink(activeNode)
-            } else {
-                // Since the active node has an edge starting with the next character, we need to
-                // either create a new leaf node, continue down the active edge, or split the
-                // current edge and create both an internal node and a leaf node.
-                val nextNode = activeNode.edges[activeEdgeChar]!!
-
-                // If the reference to the active point is non-canonical, then canonize it by
-                // stepping through the tree, and then go to the next extension of the current
-                // phase so that we can do all our steps from the basis of a canonical reference to
-                // the active point.
-                val edgeLength = nextNode.edgeLength()
-                if (activeLength >= edgeLength) {
-                    activeEdge += edgeLength
-                    activeLength -= edgeLength
-                    activeNode = nextNode
-                    continue
-                }
-
-                // If the character is already present on the edge we are creating for the next
-                // node, then the suffix is implicitly contained within the tree already, so we
-                // end the current phase. We need to add a suffix link to the active node as well,
-                // because otherwise the active point won't get to the correct place after our next
-                // insertion.
-                if (currentlyInsertedInput[nextNode.start + activeLength] == c) {
-                    activeLength++
-                    addSuffixLink(activeNode)
-                    break
-                }
-
-                // The next node we're adding will be an internal node. We add it, and create a
-                // leaf node from it whose edge corresponds to the character we're adding. We also
-                // create a suffix link for the newly added internal node.
-                val internalNode = Node(nextNode.start, nextNode.start + activeLength)
-                activeNode.edges[activeEdgeChar] = internalNode
-                internalNode.edges[c] = LeafNode()
-                nextNode.start += activeLength
-                internalNode.edges[currentlyInsertedInput[nextNode.start]] = nextNode
-                addSuffixLink(internalNode)
-            }
-
-            // Since we have completed the above conditional block, it means that we have added a
-            // new leaf node, which means that a new suffix has been made explicit within the tree.
-            // When this happens, we decrement the number of suffixes that still need to be added to
-            // the tree.
-            remainingSuffixes--
-
-            if (activeNode == rootNode && activeLength > 0) {
-                // When we insert a node from root, we decrement our active length, and pull our
-                // active edge forwards to point at the start of the next suffix we're adding.
-                activeLength--
-                activeEdge = currentlyInsertedInput.length - remainingSuffixes
-            } else {
-                // When we insert a node from an internal node, we follow its suffix link if it has
-                // one. The default suffix link for any node is root.
-                activeNode = activeNode.suffixLink()
-            }
-        }
+        // We ask the active point to add the remaining suffixes, with each suffix able to be added
+        // in O(1) time because the active point is creating and exploiting knowledge about the
+        // string's suffixes through the use of suffix links.
+        activePoint.addRemainingSuffixes(c)
     }
 
     /**
@@ -118,23 +42,17 @@ class SuffixTree {
         addChar('\u0000')
     }
 
-    private fun addSuffixLink(node: Node) {
-        suffixLinkCandidate?.linkTo(node)
-        suffixLinkCandidate = node
-    }
-
     /**
      * Finds the offsets of the given query string in the root
      */
     fun offsetsOf(queryString: String): Set<Int> {
         var i = 0
-        var node = rootNode
+        var node: Node = rootNode
         while (i < queryString.length) {
             // If we're at a leaf, then we get a match if the leaf's text matches the query string.
             // Otherwise we don't get a match.
-            if (node.edges.isEmpty()) {
-                val edgeLabel = node.edgeLabel()
-                return if (edgeLabel.startsWith(queryString)) {
+            if (node is LeafNode) {
+                return if (node.edgeLabel().startsWith(queryString)) {
                     setOf(node.suffix)
                 } else {
                     setOf()
@@ -192,28 +110,15 @@ class SuffixTree {
         val suffix = currentlyInsertedInput.length - remainingSuffixes
         var edges = TreeMap<Char, Node>()
 
-        fun suffixLink(): Node {
-            return suffixLink ?: rootNode
-        }
+        fun edgeLength(): Int = minOf(end, currentlyInsertedInput.length) - start
+
+        fun edgeLabel(): String =
+            currentlyInsertedInput.substring(start, minOf(end, currentlyInsertedInput.length))
+
+        fun suffixLink(): Node = suffixLink ?: rootNode
 
         fun linkTo(node: Node) {
             suffixLink = node
-        }
-
-        fun edgeLength(): Int {
-            return minOf(end, currentlyInsertedInput.length) - start
-        }
-
-        fun edgeLabel(): String {
-            if (start == -1) {
-                return ""
-            }
-            val endPosition = minOf(end, currentlyInsertedInput.length)
-            val chars = CharArray(endPosition - start)
-            (start until endPosition).forEach {
-                chars[it - start] = currentlyInsertedInput[it]
-            }
-            return String(chars)
         }
 
         override fun toString(): String {
@@ -247,6 +152,96 @@ class SuffixTree {
 
         override fun toString(indentationLevel: Int): String {
             return toString()
+        }
+    }
+
+    inner class ActivePoint() {
+        private var activeNode: Node = rootNode
+        private var activeLength = 0
+        private var activeEdge = 0
+
+        private var suffixLinkCandidate: Node? = null
+
+        fun addRemainingSuffixes(c: Char) {
+            // We only add suffix links within a phase, so we reset it at the start of this phase.
+            suffixLinkCandidate = null
+
+            while (remainingSuffixes > 0) {
+                if (activeLength == 0) {
+                    // If we're at a node, point our active edge at the most recently added character in
+                    // the text.
+                    activeEdge = currentlyInsertedInput.length - 1
+                }
+
+                val activeEdgeChar = currentlyInsertedInput[activeEdge]
+                if (!activeNode.edges.containsKey(activeEdgeChar)) {
+                    // If the active node doesn't yet have a child node corresponding to the next
+                    // character, add one. When we perform a leaf insertion like this, we need to add a
+                    // suffix link.
+                    activeNode.edges[activeEdgeChar] = LeafNode()
+                    addSuffixLink(activeNode)
+                } else {
+                    // Since the active node has an edge starting with the next character, we need to
+                    // either create a new leaf node, continue down the active edge, or split the
+                    // current edge and create both an internal node and a leaf node.
+                    val nextNode = activeNode.edges[activeEdgeChar]!!
+
+                    // If the reference to the active point is non-canonical, then canonize it by
+                    // stepping through the tree, and then go to the next extension of the current
+                    // phase so that we can do all our steps from the basis of a canonical reference to
+                    // the active point.
+                    val edgeLength = nextNode.edgeLength()
+                    if (activeLength >= edgeLength) {
+                        activeEdge += edgeLength
+                        activeLength -= edgeLength
+                        activeNode = nextNode
+                        continue
+                    }
+
+                    // If the character is already present on the edge we are creating for the next
+                    // node, then the suffix is implicitly contained within the tree already, so we
+                    // end the current phase. We need to add a suffix link to the active node as well,
+                    // because otherwise the active point won't get to the correct place after our next
+                    // insertion.
+                    if (currentlyInsertedInput[nextNode.start + activeLength] == c) {
+                        activeLength++
+                        addSuffixLink(activeNode)
+                        break
+                    }
+
+                    // The next node we're adding will be an internal node. We add it, and create a
+                    // leaf node from it whose edge corresponds to the character we're adding. We also
+                    // create a suffix link for the newly added internal node.
+                    val internalNode = Node(nextNode.start, nextNode.start + activeLength)
+                    activeNode.edges[activeEdgeChar] = internalNode
+                    internalNode.edges[c] = LeafNode()
+                    nextNode.start += activeLength
+                    internalNode.edges[currentlyInsertedInput[nextNode.start]] = nextNode
+                    addSuffixLink(internalNode)
+                }
+
+                // Since we have completed the above conditional block, it means that we have added a
+                // new leaf node, which means that a new suffix has been made explicit within the tree.
+                // When this happens, we decrement the number of suffixes that still need to be added to
+                // the tree.
+                remainingSuffixes--
+
+                if (activeNode == rootNode && activeLength > 0) {
+                    // When we insert a node from root, we decrement our active length, and pull our
+                    // active edge forwards to point at the start of the next suffix we're adding.
+                    activeLength--
+                    activeEdge = currentlyInsertedInput.length - remainingSuffixes
+                } else {
+                    // When we insert a node from an internal node, we follow its suffix link if it has
+                    // one. The default suffix link for any node is root.
+                    activeNode = activeNode.suffixLink()
+                }
+            }
+        }
+
+        private fun addSuffixLink(node: Node) {
+            suffixLinkCandidate?.linkTo(node)
+            suffixLinkCandidate = node
         }
     }
 
