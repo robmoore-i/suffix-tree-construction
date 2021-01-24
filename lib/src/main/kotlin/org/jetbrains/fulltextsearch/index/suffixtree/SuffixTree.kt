49b3b5ed6c5042ab16a9006154696ea6f9df244e
romo <robmoore121@gmail.com>
2021-01-24 23:05:44 +0800

    Rearrange the SuffixTree code
    Former-commit-id: 76d00180d649576cdb5b4cefb957f2bfbfe11d61

--- lib/src/main/kotlin/org/jetbrains/fulltextsearch/index/suffixtree/SuffixTree.kt
+++ lib/src/main/kotlin/org/jetbrains/fulltextsearch/index/suffixtree/SuffixTree.kt
@@ -12,7 +12,7 @@ class SuffixTree {
         fun ukkonenConstruction(input: String): SuffixTree {
             val suffixTree = SuffixTree()
             input.forEach { suffixTree.addChar(it) }
-            suffixTree.canonizeTree()
+            suffixTree.canonize()
             return suffixTree
         }
     }
@@ -33,120 +33,23 @@ class SuffixTree {
     }
 
     /**
-     * This converts the implicit suffix tree into a canonical suffix tree by adding a character
-     * that doesn't appear elsewhere in the input.
+     * Finds the offsets of the given query string within the input string by exploiting the built
+     * tree.
      */
-    private fun canonizeTree() {
-        addChar('\u0000')
+    fun offsetsOf(queryString: String): Set<Int> {
+        return rootNode.offsetsOf(queryString)
     }
 
     /**
-     * Finds the offsets of the given query string in the root
+     * This converts the implicit suffix tree into a canonical suffix tree by adding a character
+     * that doesn't appear elsewhere in the input.
      */
-    fun offsetsOf(queryString: String): Set<Int> {
-        var i = 0
-        var node: Node = rootNode
-        while (i < queryString.length) {
-            // We try to follow the edge to the next internal node. If there is no such edge, then
-            // there are no matches and we return the empty set.
-            node = (node.edges[queryString[i]] ?: return setOf())
-
-            // If the edge we just followed is longer than the remainder of the query string, then
-            // we get matches only if the edge label starts with the remainder of the query string.
-            // Otherwise we get no matches
-            if (node.edgeLength() >= (queryString.length - i)) {
-                var j = 0
-                while (j < queryString.length - i) {
-                    if (queryString[i + j] != currentlyInsertedInput[node.start + j]) {
-                        return setOf()
-                    }
-                    j++
-                }
-                return suffixesUnderSubtreeRootedAt(node)
-            }
-
-            // If the edge we just followed doesn't have an edge label matching the query string,
-            // from the current character until the end of the edge label, then there are no
-            // matches.
-            var k = 0
-            while (k < node.edgeLength()) {
-                if (queryString[i + k] != currentlyInsertedInput[node.start + k]) {
-                    return setOf()
-                }
-                k++
-            }
-
-            // We increase i by the size of the matching edge label we just crossed.
-            i += node.edgeLength()
-        }
-
-        // If we make it out of the loop, then we have consumed the full query string by traversing
-        // edges from the root. This means that all the suffixes stored within the current subtree
-        // will be prefixed by the query string.
-        return suffixesUnderSubtreeRootedAt(node)
-    }
-
-    private fun suffixesUnderSubtreeRootedAt(node: Node): Set<Int> {
-        return if (node.edges.isEmpty()) {
-            setOf(node.suffix)
-        } else {
-            node.edges.flatMap { suffixesUnderSubtreeRootedAt(it.value) }.toSet()
-        }
+    private fun canonize() {
+        addChar('\u0000')
     }
 
     override fun toString(): String {
         return "SuffixTree(rootNode={\n$rootNode\n})"
-    }
-
-    open inner class Node(var start: Int, private var end: Int) {
-        private var suffixLink: Node? = null
-
-        val suffix = currentlyInsertedInput.length - remainingSuffixes
-        var edges = mutableMapOf<Char, Node>()
-
-        fun edgeLength(): Int = minOf(end, currentlyInsertedInput.length) - start
-
-        fun suffixLink(): Node = suffixLink ?: rootNode
-
-        fun linkTo(node: Node) {
-            suffixLink = node
-        }
-
-        fun edgeLabel(): String =
-            currentlyInsertedInput.substring(start, minOf(end, currentlyInsertedInput.length))
-
-        override fun toString(): String {
-            return toString(1)
-        }
-
-        open fun toString(indentationLevel: Int): String {
-            return "Node(start=$start, end=$end, suffix=$suffix, hasLink?=${suffixLink != null}, label=${edgeLabel()}, " +
-                    "edges:${
-                        edges.map {
-                            "\n${"\t".repeat(indentationLevel)}'${it.key}'=${
-                                it.value.toString(
-                                    indentationLevel + 1
-                                )
-                            }"
-                        }
-                    })"
-        }
-    }
-
-    inner class RootNode : Node(-1, -1) {
-        override fun toString(): String {
-            return "RootNode(edges:${edges.map { "\n\t'${it.key}'=${it.value.toString(2)}" }})"
-        }
-    }
-
-    inner class LeafNode : Node(currentlyInsertedInput.length - 1, Int.MAX_VALUE / 2) {
-        override fun toString(): String {
-            return "LeafNode(start=$start, end=end, suffix=$suffix, label=${edgeLabel()})"
-        }
-
-        override fun toString(indentationLevel: Int): String {
-            return toString()
-        }
     }
 
     inner class ActivePoint {
@@ -237,6 +140,108 @@ class SuffixTree {
         private fun addSuffixLink(node: Node) {
             suffixLinkCandidate?.linkTo(node)
             suffixLinkCandidate = node
+        }
+    }
+
+    open inner class Node(var start: Int, private var end: Int) {
+        private var suffixLink: Node? = null
+
+        val suffix = currentlyInsertedInput.length - remainingSuffixes
+        var edges = mutableMapOf<Char, Node>()
+
+        fun edgeLength(): Int = minOf(end, currentlyInsertedInput.length) - start
+
+        fun suffixLink(): Node = suffixLink ?: rootNode
+
+        fun linkTo(node: Node) {
+            suffixLink = node
+        }
+
+        fun edgeLabel(): String =
+            currentlyInsertedInput.substring(start, minOf(end, currentlyInsertedInput.length))
+
+        override fun toString(): String {
+            return toString(1)
+        }
+
+        open fun toString(indentationLevel: Int): String {
+            return "Node(start=$start, end=$end, suffix=$suffix, hasLink?=${suffixLink != null}, label=${edgeLabel()}, " +
+                    "edges:${
+                        edges.map {
+                            "\n${"\t".repeat(indentationLevel)}'${it.key}'=${
+                                it.value.toString(
+                                    indentationLevel + 1
+                                )
+                            }"
+                        }
+                    })"
+        }
+    }
+
+    inner class LeafNode : Node(currentlyInsertedInput.length - 1, Int.MAX_VALUE / 2) {
+        override fun toString(): String {
+            return "LeafNode(start=$start, end=end, suffix=$suffix, label=${edgeLabel()})"
+        }
+
+        override fun toString(indentationLevel: Int): String {
+            return toString()
+        }
+    }
+
+    inner class RootNode : Node(-1, -1) {
+        fun offsetsOf(queryString: String): Set<Int> {
+            var i = 0
+            var node: Node = this
+            while (i < queryString.length) {
+                // We try to follow the edge to the next internal node. If there is no such edge, then
+                // there are no matches and we return the empty set.
+                node = (node.edges[queryString[i]] ?: return setOf())
+
+                // If the edge we just followed is longer than the remainder of the query string, then
+                // we get matches only if the edge label starts with the remainder of the query string.
+                // Otherwise we get no matches
+                if (node.edgeLength() >= (queryString.length - i)) {
+                    var j = 0
+                    while (j < queryString.length - i) {
+                        if (queryString[i + j] != currentlyInsertedInput[node.start + j]) {
+                            return setOf()
+                        }
+                        j++
+                    }
+                    return suffixesUnderSubtreeRootedAt(node)
+                }
+
+                // If the edge we just followed doesn't have an edge label matching the query string,
+                // from the current character until the end of the edge label, then there are no
+                // matches.
+                var k = 0
+                while (k < node.edgeLength()) {
+                    if (queryString[i + k] != currentlyInsertedInput[node.start + k]) {
+                        return setOf()
+                    }
+                    k++
+                }
+
+                // We increase i by the size of the matching edge label we just crossed.
+                i += node.edgeLength()
+            }
+
+            // If we make it out of the loop, then we have consumed the full query string by traversing
+            // edges from the root. This means that all the suffixes stored within the current subtree
+            // will be prefixed by the query string.
+            return suffixesUnderSubtreeRootedAt(node)
+        }
+
+        private fun suffixesUnderSubtreeRootedAt(node: Node): Set<Int> {
+            return if (node.edges.isEmpty()) {
+                setOf(node.suffix)
+            } else {
+                node.edges.flatMap { suffixesUnderSubtreeRootedAt(it.value) }.toSet()
+            }
+        }
+
+        override fun toString(): String {
+            return "RootNode(edges:${edges.map { "\n\t'${it.key}'=${it.value.toString(2)}" }})"
         }
     }
 }
